869a29adc54474f30859a79ef50da85b13f9737b
CrowleyCrowd <efrennap_023@hotmail.com>
2023-01-21 14:43:26 -0500

    capacitacion 2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse #Renderiza texto
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Pantalla principal de la práctica")
+    latest_question_list = Question.objects.order_by('-pub_date', 'question_text')[:5]
+    #select from questions order by pub_date desc limit 5;
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail (request, question_id):
+    question_to_display = Question.objects.get(pk=question_id)
